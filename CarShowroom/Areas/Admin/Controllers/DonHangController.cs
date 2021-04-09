dc81364e364965ceda7bbb4dea60010c32502e60
Tuyen <Nguyenthikimtuyeen@gmail.com>
2021-04-09 21:23:17 +0700

    dad

---
+++ CarShowroom/Areas/Admin/Controllers/DonHangController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarShowroom.Models;
+
+namespace CarShowroom.Areas.Admin.Controllers
+{
+    public class DonHangController : Controller
+    {
+        // GET: DonHang
+        public ActionResult Index()
+        {
+            CarShowroomDbContext DbContext = new CarShowroomDbContext();
+            var list = DbContext.HoaDon.ToList();
+            return View(list);
+        }
+        //  Details
+
+        //https://localhost:44353/DonHang/Details/3
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            CarShowroomDbContext DbContext = new CarShowroomDbContext();
+            var list = DbContext.ChiTietHoaDon.Where(p=>p.MaHoaDon == id).ToList();
+            return View(list);
+        }
+    }
+}
